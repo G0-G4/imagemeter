67af85c04662c1323e8535bd4f43f6b880bb7ba8
George Grishchenkov <goscha-grishenkov@yandex.ru>
2022-10-26 16:08:53 +0300

    changed children update behavior

--- editor.py
+++ editor.py
@@ -10,43 +10,53 @@ def add(parent, child):
     parent_to_children[parent].add(child)
     child_to_parent[child] = parent
 
-def get_node_flt(node):
+def get_node_elt(node, i):
     children = dpg.get_item_children(node, 1) # get children from slot 1 (mvNode_Attr_Static)
-    flt = dpg.get_item_children(children[2], 1)[0] # get 0 child of node attribute (float input)
-    return flt
+    elt = dpg.get_item_children(children[2], 1)[i] # get i child of node attribute (float input)
+    return elt
 
-def get_px(node):
-    children = dpg.get_item_children(node, 1)
-    px = dpg.get_item_children(children[2], 1)[1]
-    return px
+# def get_node_flt(node):
+#     children = dpg.get_item_children(node, 1) # get children from slot 1 (mvNode_Attr_Static)
+#     flt = dpg.get_item_children(children[2], 1)[0] # get 0 child of node attribute (float input)
+#     return flt
+
+# def get_px(node):
+#     children = dpg.get_item_children(node, 1)
+#     px = dpg.get_item_children(children[2], 1)[1]
+#     return px
+flt = 0
+px = 1
 
 def recalculate(parent_node, child_node):
-    left = get_node_flt(parent_node)
-    left_px, right_px = get_px(parent_node), get_px(child_node)
+    left_flt = get_node_elt(parent_node, flt)
+    left_px, right_px = get_node_elt(parent_node, px), get_node_elt(child_node, px)
     left_px = float(dpg.get_value(left_px))
     right_px = float(dpg.get_value(right_px))
-    len_cm = dpg.get_value(left)/left_px * right_px
+    len_cm = dpg.get_value(left_flt)/left_px * right_px
     return len_cm
 
 def link_callback(sender, app_data):
-    dpg.add_node_link(app_data[0], app_data[1], parent=sender)
     parent_node, child_node = dpg.get_item_parent(app_data[0]), dpg.get_item_parent(app_data[1]) # # search for node from input or output
-    right = get_node_flt(child_node)
+    if child_node in child_to_parent or parent_node in child_to_parent or child_node in parent_to_children:
+        return
+    dpg.add_node_link(app_data[0], app_data[1], parent=sender)
+    right_flt = get_node_elt(child_node, flt)
     len_cm = recalculate(parent_node, child_node)
     add(parent_node, child_node)
-    dpg.set_value(right, len_cm)
+    dpg.set_value(right_flt, len_cm)
 
 def input_update(sender, app_data, user_data):
     print(sender, app_data, user_data)
     node = user_data
     for child_node in parent_to_children[node]:
-        right = get_node_flt(child_node)
+        right_flt = get_node_elt(child_node, flt)
         len_cm = recalculate(node, child_node)
-        dpg.set_value(right, len_cm)
+        dpg.set_value(right_flt, len_cm)
     if node in child_to_parent:
-        left = get_node_flt(child_to_parent[node])
-        len_cm = recalculate(node, child_to_parent[node])
-        dpg.set_value(left, len_cm)
+        parent_node = child_to_parent[node]
+        right_flt = get_node_elt(node, flt)
+        len_cm = recalculate(parent_node, node)
+        dpg.set_value(right_flt, len_cm)
 
 
 def add_node():
